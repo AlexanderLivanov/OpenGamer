0c048c5617999db4be725ce3a98ce9be16471d79
AlexanderLivanov <help.underlinegroup@gmail.com>
2024-06-10 21:32:39 +0300

    1

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>OpenGamer</title>
+</head>
+
+<body>
+    <div class="banner">
+        <img src="img/prefabs/main-banner.jpeg" alt="">
+        <div class="photo-text">
+            <h1>Здесь будет что-то новое!</h1>
+            <h3>Оно обязательно здесь будет!</h3>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-arrow-down" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                <path d="M8 12l4 4" />
+                <path d="M12 8v8" />
+                <path d="M16 12l-4 4" />
+            </svg>
+        </div>
+    </div>
+    <div class="gallery-block">
+        <div class="left-arrow">
+            <-
+        </div>
+        <div class="photo-content">
+            <img src="img/prefabs/gallery-1.jpg" alt="">
+        </div>
+        <div class="right-arrow">
+            ->
+        </div>
+    </div>
+</body>
+
+</html>
